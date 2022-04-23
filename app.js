a3301cbd29bfe50c4bcd074806edce056a1f8c12
Lucas2294 <lucasbuyatti94@gmail.com>
2022-04-23 12:01:55 -0300

    probando fetch

--- app.js
+++ app.js
@@ -12,9 +12,25 @@ class Piloto {
 const pUno = document.querySelector("#pPodioUno");
 const pDos = document.querySelector("#pPodioDos");
 const pTres = document.querySelector("#pPodioTres");
+const todos = document.querySelector("#todos");
 
 class APP {
     cargarPuestos() {
-        
+
     }
+}
+
+function cargarJSON() {
+    fetch('competidor.json')
+        .then(res => res.json() )
+        .then(data => {
+            
+            console.log(data);
+            data.forEach(piloto => {
+                todos.innerHTML = `hola ${piloto.nombre}`
+                console.log(piloto.nombre)
+            });
+            
+        })
+        .catch(error => console.log(error) )
 }
