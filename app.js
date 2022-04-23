2ec1fd938116e1bc0b8a7e887e36a49101d22729
Lucas2294 <lucasbuyatti94@gmail.com>
2022-04-23 09:29:06 -0300

    galería 1 css

---
+++ app.js
@@ -0,0 +1,20 @@
+
+class Piloto {
+    constructor(nombre, puntos, puesto, localidad) {
+        this.nombre = nombre;
+        this.puntos = puntos;
+        this.puesto = puesto;
+        this.localidad = localidad;
+    }
+}
+
+// Variables doc
+const pUno = document.querySelector("#pPodioUno");
+const pDos = document.querySelector("#pPodioDos");
+const pTres = document.querySelector("#pPodioTres");
+
+class APP {
+    cargarPuestos() {
+        
+    }
+}
